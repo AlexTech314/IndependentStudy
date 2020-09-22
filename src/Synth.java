52017e0bfcc4ac93702de0123b1b748a6bc0e26a
AlexTech314 <alex.s@wustl.edu>
2020-09-22 18:00:04 -0500

    made gui

---
+++ src/Synth.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class Synth {
+	
+	private JFrame frame = new JFrame("Synth");
+	
+	Synth() {
+		frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		frame.setSize(613, 357);
+		frame.setResizable(false);
+		frame.setLayout(null);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
+
+}
